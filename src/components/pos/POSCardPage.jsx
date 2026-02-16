c5ff475359300ab15fd8512492f6df3b768d3491
FRANCIS007 <francisnujimem@gmail.com>
2026-02-16 01:05:57 +0100

    UPDATE  deployment

--- src/components/pos/POSCardPage.jsx
+++ src/components/pos/POSCardPage.jsx
@@ -40,7 +40,7 @@ const Calculator = () => {
   ];
 
   return (
-    <div className="calculator" style={{ width: 297, height: 300, display: "flex", flexDirection: "column", padding: "2px", boxSizing: "border-box" }}>
+    <div className="calculator" style={{ width: 300, height: 300, display: "flex", flexDirection: "column", padding: "2px", boxSizing: "border-box" }}>
       <input
         className="calc-display"
         type="text"
