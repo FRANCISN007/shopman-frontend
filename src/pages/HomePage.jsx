c283c6b9761fa432a49c8ebb988784a4a7d27e70
FRANCIS007 <francisnujimem@gmail.com>
2026-03-28 00:40:38 +0100

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
-import { WELCOME_ADDRESS } from "../config/constants";
+
 
 const HomePage = () => {
   const navigate = useNavigate();
